d30e0f4f927739e61aed686db7244e5e40290dfb
DeepikaPatel2002 <deepikapatelmca.2002@gmail.com>
2026-03-06 21:07:32 +0530

    adding second file

--- demo.js
+++ demo.js
@@ -1,2 +1,14 @@
 
-console.log("adding second file");
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.color = 'orange';
+header.style.borderBottom = '3px solid orange';
+
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World';
+
+const basketHeading = document.getElementById('basket-heading');
+basketHeading.style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML='<p> Please visit us again</p>'
